089b1f9e39f07c73cc7b8bddb02e33ef48098c2c
SanhTuan <sanhtuanst1029@gmail.com>
2023-08-12 15:30:51 +0700

    fix deployment

--- app.py
+++ app.py
@@ -4,8 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    html = f"<h3>Hello, I'am Sanh Tuan. Udacity Capstone Project-Cloud DevOps Engineer</h3>"
-    return html.format(format)
+    return "<h3>Hello, I'am Sanh Tuan. This is Udacity Capstone Project-Cloud DevOps Engineer!</h3>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True) 
